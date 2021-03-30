f33e06b2310d79df3ca1adfc3de1a822a19c6b4b
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-03-30 15:07:47 -0300

    definindo headline e espaço adsense (#6)

--- src/components/AdsenseBanner/styles.ts
+++ src/components/AdsenseBanner/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.section``;
+export const Container = styled.section`
+text-align:center;
+img{
+    height:22.5rem;
+    width:97.33vw;
+    margin-top:6.25rem;
+    margin-bottom:6.25rem;
+}
+
+`;
--- src/components/HeadlineNews/index.tsx
+++ src/components/HeadlineNews/index.tsx
@@ -1,23 +1,46 @@
-import { Container, HeadlineGroup, MainHeadline } from "./styles";
+import { Link } from "react-router-dom";
+import { Container } from "./styles";
 
 export function HeadlineNews() {
   return (
-    <Container>
-      {/* <MainHeadline></MainHeadline>
-      <HeadlineGroup>
-        <figure>
-          <img src="" alt="" />
-          <figcaption></figcaption>
-        </figure>
-        <figure>
-          <img src="" alt="" />
-          <figcaption></figcaption>
-        </figure>
-        <figure>
-          <img src="" alt="" />
-          <figcaption></figcaption>
-        </figure>
-      </HeadlineGroup> */}
-    </Container>
+  <Container>
+      <Link to="noticia/id" className="headline-notice">
+        <img 
+          src="https://image.freepik.com/fotos-gratis/mulher-fazendo-uma-videochamada-em-um-laptop_53876-95835.jpg" 
+          alt="Grupo de pessoas em reunião"
+        />
+        <h1>A Revolução 4.0 e seus Impactos
+           na Indústria de Softwares</h1>
+      </Link>
+      <div className="headline-others">
+        <Link to="noticia/id" className="headline-card">
+          <img 
+            src="https://images.unsplash.com/photo-1425421669292-0c3da3b8f529?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80" 
+            alt=""
+          />
+          <p>
+            Senado prevê um gasto de 34,2 bi de com novo auxílio neste semestre
+          </p>
+        </Link>
+        <Link to="noticia/id" className="headline-card">
+          <img 
+            src="https://images.unsplash.com/photo-1561057160-ce83b1bd72f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" 
+            alt=""
+          />
+          <p>
+            Seminário discute o efeito da pandemia em pessoas LGBTQI+
+          </p>
+        </Link>
+        <Link to="noticia/id" className="headline-card">
+          <img 
+            src="https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=633&q=80" 
+            alt=""
+          />
+          <p>
+            Restrições e cuidados para quem vai viajar durante a pandemia
+          </p>
+        </Link>
+      </div>
+  </Container>
   );
 }
