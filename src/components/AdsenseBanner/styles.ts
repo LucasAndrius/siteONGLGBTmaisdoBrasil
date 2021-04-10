73bed0a81d62dc16a6b34cb8a449881cf7e85821
Juliane Pires <36718319+JulianePires@users.noreply.github.com>
2021-04-10 13:03:33 -0300

    Lucas a (#10) (#11)
    * definindo headline e espaço adsense
    
    * com arquivo add incluso
    
    * new file
    
    Co-authored-by: GuilhermeM06 <marquez.guilherme@gmail.com>
    Co-authored-by: Juliane Pires <36718319+JulianePires@users.noreply.github.com>
    
    Co-authored-by: Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
    Co-authored-by: GuilhermeM06 <marquez.guilherme@gmail.com>

--- src/components/AdsenseBanner/styles.ts
+++ src/components/AdsenseBanner/styles.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  width: 100%;
-  text-align: center;
-  img {
-    width: 100%;
-    margin-top: 3.25rem;
-    margin-bottom: 3.25rem;
-  }
+text-align:center;
+img{
+    height:22.5rem;
+    width:97.33vw;
+    margin-top:6.25rem;
+    margin-bottom:6.25rem;
+}
 `;
