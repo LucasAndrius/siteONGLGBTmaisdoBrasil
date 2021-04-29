a1648df95e241d18423dc5c6c77f317df7609a8b
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-29 13:36:01 -0300

    correção de importação na agenda

--- src/pages/Agenda/index.tsx
+++ src/pages/Agenda/index.tsx
@@ -1,10 +1,10 @@
 import { Header } from "../../components/Header";
 import { BannerNewsletter } from "../../components/BannerNewsletter";
-import { FooterArea } from "../../components/Footer";
 
 import { Container } from "./styles";
 
 import constructionImg from "../../assets/construcao.png";
+import { FooterArea } from "../../components/Footer";
 
 export function Agenda() {
   return (
