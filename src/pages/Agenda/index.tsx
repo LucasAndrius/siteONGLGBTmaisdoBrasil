ce841e187041a2c006ce65c0b98ce1bff6f95c66
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-29 13:56:44 -0300

    removendo componentes de agenda

--- src/pages/Agenda/index.tsx
+++ src/pages/Agenda/index.tsx
@@ -1,20 +1,9 @@
-import { Header } from "../../components/Header";
-import { BannerNewsletter } from "../../components/BannerNewsletter";
 
 import { Container } from "./styles";
-
-import constructionImg from "../../assets/construcao.png";
 
 export function Agenda() {
   return (
     <Container>
-      <Header pageName="agenda" />
-      <BannerNewsletter pageName="agenda" />
-      <img
-        src={constructionImg}
-        alt="Página em construção"
-        className="construction"
-      />
       <h2>Página em Construção</h2>
     </Container>
   );
