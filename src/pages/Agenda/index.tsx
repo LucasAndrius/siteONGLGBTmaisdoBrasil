3b3649f496ea88ccf45cb0d849107ab3f7c47355
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-29 13:59:43 -0300

    adicionando componentes a agenda

--- src/pages/Agenda/index.tsx
+++ src/pages/Agenda/index.tsx
@@ -1,10 +1,23 @@
+import { Header } from "../../components/Header";
+import { BannerNewsletter } from "../../components/BannerNewsletter";
+import { FooterArea } from "../../components/Footer";
 
 import { Container } from "./styles";
+
+import constructionImg from "../../assets/construcao.png";
 
 export function Agenda() {
   return (
     <Container>
+      <Header pageName="agenda" />
+      <BannerNewsletter pageName="agenda" />
+      <img
+        src={constructionImg}
+        alt="Página em construção"
+        className="construction"
+      />
       <h2>Página em Construção</h2>
+      <FooterArea pageName="agenda" />
     </Container>
   );
 }
